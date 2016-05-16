10271dc030dacb12ee4a93e0dc86baf6c595c557
Yash Shah <mail@yashshah.com>
2016-05-16 18:17:00 +0530

    Suggest multiple dates

--- api/scheduler.js
+++ api/scheduler.js
@@ -10,6 +10,7 @@ require('moment-range');
 // Configure the preference of the User
 const USER_TIMEZONE = "Asia/Kolkata";
 const NUMBER_OF_TIME_SLOTS_REQUIRED = 3;
+const NUMBER_OF_DATES_REQUIRED = 4;
 // Set the user's workable hours
 const CAL_AVAILIBILITY_TIME_START = moment.tz("08:00", 'HH:mm', USER_TIMEZONE);
 const CAL_AVAILIBILITY_TIME_END = moment.tz("22:00", 'HH:mm', USER_TIMEZONE);
@@ -20,22 +21,24 @@ module.exports = function(req, res) {
   // Get the latitue and longitude for the city user selected
   locations.autocomplete({ input: req.param('text'), types: "(cities)" }, function(err, response) {
     locations.details({ placeid: response.predictions[0].place_id }, function(err, response) {
-      // Get the timezone of the 
+      // Get the timezone of the attendee
       request('https://api.timezonedb.com/?lat=' + response.result.geometry.location.lat + '&lng=' + response.result.geometry.location.lng + '&key=I16KGCYBRXQ8&format=json', function(error, response, body) {
         if (!error && response.statusCode == 200) {
-          var userStartDate = CAL_AVAILIBILITY_TIME_START.add(1, "days")
-          var userEndDate = CAL_AVAILIBILITY_TIME_END.add(1, "days")
-
+          var userStartDate = CAL_AVAILIBILITY_TIME_START.clone().add(1, "days")
+          var userEndDate = CAL_AVAILIBILITY_TIME_END.clone().add(NUMBER_OF_DATES_REQUIRED, "days")
+          var attendeeTimeZone = JSON.parse(body).zoneName
           var getRequestObject = {
             type: appbase.config.type,
             id: 'X1'
           };
+          // Get the tokens from Appbase
           appbase.ref.get(getRequestObject).on('data', function(results) {
             var tokens = results._source;
             google.oauth2Client.setCredentials({
               'access_token': tokens.access_token,
               'refresh_token': tokens.refresh_token
             });
+            // Get the events from the google calendar
             google.calendar.events.list({
               auth: google.oauth2Client,
               calendarId: 'primary',
@@ -48,12 +51,17 @@ module.exports = function(req, res) {
               if (err) {
                 console.log('The API returned an error: ' + err);
               }
-              // Get the feasiable time slots in the attendee's timezone
-              feasibleTimeSlots = getFeasibleTimeSlots(JSON.parse(body).zoneName, response.items);
               // Using Mixmax SDK for displaying the slots in the Mixmax
-              var html = "Let me know which time works for you tomorrow: " + feasibleTimeSlots.map(function(response) {
-                return " " + response;
-              })
+              var html = "Let me know which time works for you in " + moment.tz(attendeeTimeZone).format('z') + ": </br>";
+              // Get feasiable time slots for preferred number of dates
+              for (k = 0; k < NUMBER_OF_DATES_REQUIRED; k++) {
+                // Get the feasiable time slots in the attendee's timezone
+                feasibleTimeSlots = getFeasibleTimeSlots(attendeeTimeZone, response.items, userStartDate);
+                html = html + userStartDate.format('ddd, MMM Do') + ': ' + feasibleTimeSlots.map(function(response) {
+                  return " " + response;
+                }) + "</br>"
+                userStartDate = userStartDate.add(1, "days")
+              }
               res.json({
                 body: html,
                 raw: true
@@ -69,39 +77,42 @@ module.exports = function(req, res) {
 }
 
 // Given the timezone, get the most feasiable time slots
-function getFeasibleTimeSlots(attendeeTimeZone, busySlots) {
+function getFeasibleTimeSlots(attendeeTimeZone, busySlots, userStartDate) {
   var feasibleHours = {}; // New object
   var userAvailabilityInHours = moment.duration(CAL_AVAILIBILITY_TIME_END.diff(CAL_AVAILIBILITY_TIME_START)).asHours();
-  var attendeeAvailibilityStartTime = moment.tz("08:00", 'HH:mm', attendeeTimeZone).add(1, 'days');
-  var attendeeAvailibilityEndTime = moment.tz("22:00", 'HH:mm', attendeeTimeZone).add(1, 'days');
-  var userTimeInAttendeeTimeZone = CAL_AVAILIBILITY_TIME_START.clone().tz(attendeeTimeZone);
+  var attendeeAvailibilityStartTime = moment.tz("08:00", 'HH:mm', attendeeTimeZone);
+  var attendeeAvailibilityEndTime = moment.tz("22:00", 'HH:mm', attendeeTimeZone);
+
   if (busySlots.length == 0) {
     console.log('No upcoming events found.');
   } else {
     console.log('Upcoming events: ', busySlots.length);
-    for (var i = 0; i < busySlots.length; i++) {
-      var event = busySlots[i];
-      var event_id = event.id;
-      var summary = event.summary;
-      var start = event.start.dateTime || event.start.date;
-      var end = event.end.dateTime || event.end.date;
-      var range = moment.range(start, end);
-      console.log(start, end, summary)
-      // Loop through all the time slots where user is available and if it belongs to Attendee workable hours
-      for (j = 0; j < userAvailabilityInHours * 2; j++) {
-        if (userTimeInAttendeeTimeZone.format('HHmm') >= attendeeAvailibilityStartTime.format('HHmm') && userTimeInAttendeeTimeZone.format('HHmm') <= attendeeAvailibilityEndTime.format('HHmm')) {
-          if (range.contains(userTimeInAttendeeTimeZone))
-            console.log("Removed: ", userTimeInAttendeeTimeZone.format('HH:mm'))
-          else
-            feasibleHours[userTimeInAttendeeTimeZone.format('HH:mm z')] = 1;
+    var userTimeInAttendeeTimeZone = userStartDate.clone().tz(attendeeTimeZone);
+    // Loop through all the time slots where user is available and if it belongs to Attendee workable hours
+    for (var i = 0; i < userAvailabilityInHours * 2; i++) {
+      if (userTimeInAttendeeTimeZone.format('HHmm') >= attendeeAvailibilityStartTime.format('HHmm') && userTimeInAttendeeTimeZone.format('HHmm') <= attendeeAvailibilityEndTime.format('HHmm')) {
+        var slotFreeFlag = 1;
+        for (var j = 0; j < busySlots.length; j++) {
+          // var event = busySlots[j];
+          // var event_id = event.id;
+          // var summary = event.summary;
+          var start = event.start.dateTime || event.start.date;
+          var end = event.end.dateTime || event.end.date;
+          var range = moment.range(start, end);
+          // console.log(start, end, userTimeInAttendeeTimeZone.format(), range.contains(userTimeInAttendeeTimeZone))
+          if (range.contains(userTimeInAttendeeTimeZone)) {
+            slotFreeFlag = 0
+              // console.log("Removed: ", userTimeInAttendeeTimeZone.format('HH:mm'))
+            break;
+          }
         }
-        // Increasing the time by 30 minutes in each iteration
-        userTimeInAttendeeTimeZone = userTimeInAttendeeTimeZone.add(30, "minutes");
+        if (slotFreeFlag) {
+          feasibleHours[userTimeInAttendeeTimeZone.format('hh:mm a')] = 1;
+        }
       }
-      // addAppointment(event_id, summary, start, end);
-
+      // Increasing the time by 30 minutes in each iteration
+      userTimeInAttendeeTimeZone = userTimeInAttendeeTimeZone.add(30, "minutes");
     }
-
   }
   return calculatFeasibleTimeSlots(feasibleHours);
 }
@@ -120,7 +131,7 @@ function calculatFeasibleTimeSlots(feasibleHours) {
   slotGroupLength = (feasibleHoursArrayLength / NUMBER_OF_TIME_SLOTS_REQUIRED);
   // Break down the slots into groups based on number of time suggestions user wants to send to Attendee
   // It enables user to provide attendee with variety of hours at different time of the day
-  for (i = 0; i < NUMBER_OF_TIME_SLOTS_REQUIRED; i++) {
+  for (var i = 0; i < NUMBER_OF_TIME_SLOTS_REQUIRED; i++) {
     // Calculate the start and end index number based on the size of each group
     slotGroupStartingIndex = Math.round(i * slotGroupLength);
     // Check if Index is out of bound then end index should be feasibleHoursArrayLength
@@ -132,5 +143,8 @@ function calculatFeasibleTimeSlots(feasibleHours) {
       // Pick the time slot from each slot group
     feasibleSlots.push(sortedSlotGroup[Math.floor(Math.random() * sortedSlotGroup.length)][0]);
   }
+  feasibleSlots.sort(function(a, b) {
+    return new Date('1970/01/01 ' + a) - new Date('1970/01/01 ' + b);
+  });
   return feasibleSlots;
 };
